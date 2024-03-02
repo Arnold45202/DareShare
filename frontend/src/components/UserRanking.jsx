40a17b62dc7dd1070a9f384b34dbcce6b6cf9d2d
Louis Nguyen <ndmn004@gmail.com>
2024-03-02 20:14:07 +1100

    leaderboard

--- frontend/src/components/UserRanking.jsx
+++ frontend/src/components/UserRanking.jsx
@@ -7,7 +7,7 @@ const UserRanking = (props) => {
   return (
     <Box sx={{ display: 'flex', justifyContent: 'space-between', borderBottom: 1, borderColor: 'rgb(121, 19, 168)', p: 1 }}>
       <Typography variant='h6' sx={{ color: 'white' }}>
-        {props.rank}. {props.username}
+        {props.rank}. <img src={props.img} alt='' style={{ borderRadius: '100px', height: '25px', width: '25px', position: 'relative', top: 7, marginLeft: 5, marginRight: 5 }} /> {props.username}
       </Typography>
       <Typography variant='h6' sx={{ color: 'white' }}>
         {props.point} <img src={mask} alt='' style={{ width: '30px', height: '30px', position: 'relative', top: 7 }} />
