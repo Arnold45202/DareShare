79f1c42c3846ebc842af318b36b8e46eb54ea50a
Khye Jac <lowkhyejac@gmail.com>
2024-03-02 21:23:57 +1100

    hey

--- frontend/src/pages/upload.jsx
+++ frontend/src/pages/upload.jsx
@@ -4,7 +4,7 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Icon from '@mui/material/Icon';
 import CameraAltIcon from '@mui/icons-material/CameraAlt';
-import devil from '../assets/devil.png';
+import logo from '../assets/ds-logo.png';
 import { Button } from '@mui/material';
 
 
@@ -20,27 +20,31 @@ function Upload() {
         <>
             <Box sx={{ pt: 2, px: 5, textAlign: 'center' }}>
                 <img
-                    src = {devil}
-                    style={{ width: '70%', height: 'auto', display: 'block', margin: 'auto', marginBottom: '1rem' }}
+                    src = {logo}
+                    style={{ width: '45%', height: 'auto', display: 'block', margin: 'auto', marginBottom: '1rem' }}
                 />
-                <Box sx={{ background: 'rgb(121, 19, 168)', borderRadius: '10px', textAlign: 'center', p: 1 }}>
-                    <Typography variant='h4' color={'white'}>
-                        UPLOAD A DARE
+                <Box sx={{px: 2}}>
+                    <Typography variant='h4' sx={{ color: 'white', textAlign: 'center', fontWeight: 'bold' }}>
+                        UPLOAD YOUR DARE
                     </Typography>
                 </Box>
-                <Button
-                onClick = {handleSubmit}
-                style = {{
-                    color: 'white',
-                    '&:hover': {
-                        backgroundColor: 'rgb(100, 19, 168)', // Button hover color
-                    },
-                    transform: 'scale(3.0)',
-                    py: 2,
-    
-                }}
-                ><CameraAltIcon />
-                </Button>
+                    <Box sx = {{px : 4}}>
+                        <Button
+                        onClick = {handleSubmit}
+                        
+                        sx = {{
+                            color: 'white',
+                            transform: 'scale(4.0)',
+                            py: 0.5,
+                            px: 0.2,
+                            top: 12                   
+                        }}
+                        ><CameraAltIcon />
+                        </Button>
+                    </Box>
+                <Typography variant = 'h8' sx = {{ color: 'white', textAlign: 'center'}}>
+                    Take a video
+                </Typography>
             
             </Box>
         </>
