a8e3802ceadc40fee76b4a9889ba1677a76f89e4
Louis Nguyen <ndmn004@gmail.com>
2024-03-02 15:17:12 +1100

    SubmitPage

--- frontend/src/pages/submit.jsx
+++ frontend/src/pages/submit.jsx
@@ -1,66 +1,120 @@
-import * as React from 'react';
+import { useState } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import IconButton from '@mui/material/IconButton';
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import Typography from '@mui/material/Typography';
 import '../styleSheets/submit.css';
-import { Link, useNavigate } from 'react-router-dom';
-
-// function Submit() {
-//     return (
-
-//         <>
-//             <h1>Submit a dare!</h1>
-//             <Box
-//                 component="form"
-//                 sx={{
-//                 '& > :not(style)': { m: 5, width: '40ch', height: 10},
-//                 }}
-//                 noValidate
-//                 autoComplete="off"
-//             >
-//                 <TextField id="outlined-basic" label="Enter your dare!" variant="outlined" />
-//             </Box>
-
-//             <IconButton aria-label="delete" size="large" onClick={() => <Meow/>}>
-//                 <CheckCircleOutlineIcon fontSize="inherit"/>
-//             </IconButton>
-//         </>
-//     );
-// }
+import { useNavigate } from 'react-router-dom';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import { renderTimeViewClock } from '@mui/x-date-pickers/timeViewRenderers';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
+import dayjs from 'dayjs';
 
 function Submit() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const [daretext, setDaretext] = useState('');
+  const [time, setTime] = useState(dayjs());
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        navigate('/homepage');
-    }
-      
-    return (
-        <div className = "submit-container">
-        <h1 className = "submit-title">Submit a dare!</h1>
-        <form className = "submit-form" onSubmit = {handleSubmit}>
-            <div className = "form-group">
-                <Box
-                    component="form"
-                    sx={{
-                    '& > :not(style)': { m: 5, width: '40ch', size: "large"},
-                    }}
-                    noValidate
-                    autoComplete="off"
-                >
-                    <TextField id="outlined-basic" label="Enter your dare!" variant="outlined" />
-                </Box>
-            </div>
-            <IconButton aria-label="delete" size="large" onClick={() => <Meow/>}>
-                    <CheckCircleOutlineIcon fontSize="inherit"/>
-                </IconButton>
-        </form>
-        </div>
-    )
+  function handleSubmit(event) {
+    event.preventDefault();
+    navigate('/homepage');
+  }
+
+  return (
+    <>
+      <Box sx={{ pt: 2, px: 5, textAlign: 'center' }}>
+        <img
+          src='https://cdn.discordapp.com/attachments/1207441349754884116/1213132242810052639/image.png?ex=65f45c71&is=65e1e771&hm=5f9ecb2b8953ef6622466df709a530a1ac65afe8aac34ec9e009f0e73c0e8221&'
+          style={{ width: '70%', height: 'auto', display: 'block', margin: 'auto', marginBottom: '1rem' }}
+        />
+        <Box sx={{ background: 'rgb(121, 19, 168)', borderRadius: '50px', textAlign: 'center', p: 1 }}>
+          <Typography variant='h4' color={'white'}>
+            Submit a dare!
+          </Typography>
+        </Box>
+
+        <Box sx={{ mx: 2, textAlign: 'center', my: '2rem', backgroundColor: 'white', p: 2, borderRadius: '20px' }}>
+          <TextField
+            id='outlined-dare'
+            size='large'
+            label='Enter your dare!'
+            variant='outlined'
+            value={daretext}
+            onChange={(e) => setDaretext(e.target.value)}
+            sx={{
+              '& .MuiInputBase-root': {
+                backgroundColor: 'white',
+              },
+              '& .MuiOutlinedInput-root': {
+                '& fieldset': {
+                  borderColor: 'rgb(121, 19, 168)', // Default border color
+                },
+                '&:hover fieldset': {
+                  borderColor: 'rgb(121, 19, 168)', // Border color on hover (You can adjust this color)
+                },
+                '&.Mui-focused fieldset': {
+                  borderColor: 'rgb(121, 19, 168)', // Border color when focused
+                },
+                '& .MuiInputLabel-root': {
+                  // Targeting the label
+                  color: 'rgb(121, 19, 168)', // Label color
+                },
+                '& .MuiInputLabel-root.Mui-focused': {
+                  // Specifically targeting the focused state label
+                  color: 'rgb(121, 19, 168)', // Label color when the TextField is focused
+                },
+                width: '18rem',
+              },
+            }}
+          />
+        </Box>
+        <Box sx={{ mx: 2, textAlign: 'center', my: '2rem', backgroundColor: 'white', p: 2, borderRadius: '20px' }}>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <TimePicker
+              label='Choose Time For Your Dare'
+              viewRenderers={{
+                hours: renderTimeViewClock,
+                minutes: renderTimeViewClock,
+                seconds: renderTimeViewClock,
+              }}
+              sx={{
+                '& .MuiInputBase-root': {
+                  backgroundColor: 'white',
+                },
+                '& .MuiOutlinedInput-root': {
+                  '& fieldset': {
+                    borderColor: 'rgb(121, 19, 168)', // Default border color
+                  },
+                  '&:hover fieldset': {
+                    borderColor: 'rgb(121, 19, 168)', // Border color on hover (You can adjust this color)
+                  },
+                  '&.Mui-focused fieldset': {
+                    borderColor: 'rgb(121, 19, 168)', // Border color when focused
+                  },
+                  width: '18rem',
+                },
+              }}
+              value={time}
+              onAccept={(newValue) => setTime(newValue)}
+            />
+          </LocalizationProvider>
+        </Box>
+      </Box>
+
+      <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: '3rem', mx: 2 }}>
+        <IconButton aria-label='delete' size='large' sx={{ color: 'white', p: 3 }} onClick={handleSubmit}>
+          <HighlightOffIcon sx={{ fontSize: '8rem', color: 'red' }} />
+        </IconButton>
+        <IconButton aria-label='check' size='large' sx={{ color: 'white', p: 3 }} onClick={handleSubmit}>
+          <CheckCircleOutlineIcon sx={{ fontSize: '8rem', color: 'green' }} />
+        </IconButton>
+      </Box>
+    </>
+  );
 }
-
 
 export default Submit;
