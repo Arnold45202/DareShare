433f28db34b04413d2285c2838839071b16affe4
Louis Nguyen <ndmn004@gmail.com>
2024-03-02 23:19:35 +1100

    discover

--- frontend/src/pages/discover.jsx
+++ frontend/src/pages/discover.jsx
@@ -7,14 +7,6 @@ import { inputLabelClasses } from '@mui/material/InputLabel';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 
-
-
-
-
-
-
-
-
 function srcset(image, size, rows = 1, cols = 1) {
   return {
     src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
