9ddd930d5373596a2144d6ad060868e1a5092035
Thant Zin Tun <zyler@Thants-MacBook-Air.local>
2024-03-03 12:43:16 +1100

    Change style

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -80,35 +80,37 @@ function Home() {
 			</div>
 
 			<div className="dare-info">
-				Your DARE: Pay for someone's meal
+				<span className='dare-title'>Today's DARE</span><br></br>
+				<span className='dare-mission' style={{fontSize: "16px"}}>Pay for someone's meal</span>
 			</div>
 			<div className="upload-dare-button">
 				<Link to="/submit" className="upload-btn"><p1>UPLOAD YOUR DARE</p1></Link>
 			</div>
+			<div className="transition-20-block">
+				<div className="transition-20"></div>
+				<div className="transition-19"></div>
+				<div className="transition-18"></div>
+				<div className="transition-17"></div>
+				<div className="transition-16"></div>
+				<div className="transition-15"></div>
+				<div className="transition-14"></div>
+				<div className="transition-13"></div>
+				<div className="transition-12"></div>
+				<div className="transition-11"></div>
+				<div className="transition-10"></div>
+				<div className="transition-9"></div>
+				<div className="transition-8"></div>
+				<div className="transition-7"></div>
+				<div className="transition-6"></div>
+				<div className="transition-5"></div>
+				<div className="transition-4"></div>
+				<div className="transition-3"></div>
+				<div className="transition-2"></div>
+				<div className="transition-1"></div>
+			</div>
 		</div>
 
-		<div className="transition-20-block">
-			<div className="transition-20"></div>
-			<div className="transition-19"></div>
-			<div className="transition-18"></div>
-			<div className="transition-17"></div>
-			<div className="transition-16"></div>
-			<div className="transition-15"></div>
-			<div className="transition-14"></div>
-			<div className="transition-13"></div>
-			<div className="transition-12"></div>
-			<div className="transition-11"></div>
-			<div className="transition-10"></div>
-			<div className="transition-9"></div>
-			<div className="transition-8"></div>
-			<div className="transition-7"></div>
-			<div className="transition-6"></div>
-			<div className="transition-5"></div>
-			<div className="transition-4"></div>
-			<div className="transition-3"></div>
-			<div className="transition-2"></div>
-			<div className="transition-1"></div>
-		</div>
+		
 
 		<div className="posts-container"> 
 			{posts.map(post => (
@@ -117,11 +119,11 @@ function Home() {
 		</div>
 
 		<div className="navigation">
-			<div><Link to="/home" className="navigation-btn"><SettingsIcon sx={{color: 'white', height: '40px', width: '40px'}}/></Link></div>
-			<div><Link to="/upload" className="navigation-btn"><img src={DevilIcon} alt="" style={{height: '40px', width: '40px'}}/></Link></div>
-			<div><Link to="/discover" className="navigation-btn"><SearchIcon sx={{color: 'white', height: '40px', width: '40px'}}/></Link></div>
-			<div><Link to="/leaderboard" className="navigation-btn"><LeaderboardIcon sx={{color: 'white', height: '40px', width: '40px'}}/></Link></div>
-			<div><Link to="/home" className="navigation-btn"><PersonIcon sx={{color: 'white', height: '40px', width: '40px'}}/></Link></div>
+			<div><Link to="/home" className="navigation-btn"><SettingsIcon sx={{color: 'white', height: '40px', width: '40px', borderRadius: '12px'}}/></Link></div>
+			<div><Link to="/upload" className="navigation-btn"><img src={DevilIcon} alt="" style={{height: '40px', width: '40px',  borderRadius: '12px'}}/></Link></div>
+			<div><Link to="/discover" className="navigation-btn"><SearchIcon sx={{color: 'white', height: '40px', width: '40px', borderRadius: '12px'}}/></Link></div>
+			<div><Link to="/leaderboard" className="navigation-btn"><LeaderboardIcon sx={{color: 'white', height: '40px', width: '40px',borderRadius: '12px'}}/></Link></div>
+			<div><Link to="/home" className="navigation-btn"><PersonIcon sx={{color: 'white', height: '40px', width: '40px',  borderRadius: '12px'}}/></Link></div>
 		</div>
 	</div>
   )
